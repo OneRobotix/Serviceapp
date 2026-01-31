2509a2bd719dc0407fc4e590558c8669600a9a5b
OneRobotix <onerobotixtablet@gmail.com>
2026-01-31 17:07:57 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // השינוי הוא כאן: נקודה ולוכסן בלבד
-  // זה אומר: "חפש את הקבצים באותה תיקייה שאני נמצא בה"
   base: './', 
 })
